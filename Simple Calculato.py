3a1b047781891abd3b8fe7d8a8263b329a567364
rougeuser <160385462+rougeuser@users.noreply.github.com>
2024-03-18 20:24:37 +0530

    Simple Calculato.py

---
+++ Simple Calculato.py
@@ -0,0 +1,18 @@
+def calculator(num1, num2, operator):
+    if operator == '+':
+        return num1 + num2
+    elif operator == '-':
+        return num1 - num2
+    elif operator == '*':
+        return num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "Error: Division by zero"
+
+if __name__ == "__main__":
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    operator = input("Enter the operator (+, -, *, /): ")
+    print("Result:", calculator(num1, num2, operator))
